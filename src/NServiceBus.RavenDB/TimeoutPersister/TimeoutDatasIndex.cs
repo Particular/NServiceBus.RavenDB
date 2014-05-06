18e1c9cf133fbf5bc7b10f19a0f292c34817514b
Itamar Syn-Hershko <itamar@code972.com>
2014-05-06 23:10:51 +0300

    An index for querying TimeoutData

---
+++ src/NServiceBus.RavenDB/TimeoutPersister/TimeoutDatasIndex.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus.RavenDB.TimeoutPersister
+{
+    using System.Linq;
+    using Raven.Client.Indexes;
+    using Timeout.Core;
+
+    public class TimeoutDatasIndex : AbstractIndexCreationTask<TimeoutData>
+    {
+        public TimeoutDatasIndex()
+        {
+            Map = docs => from doc in docs
+                select new
+                       {
+                           doc.Time,
+                           doc.SagaId,
+                           doc.OwningTimeoutManager,
+                       };
+
+            DisableInMemoryIndexing = true;
+        }
+    }
+}
