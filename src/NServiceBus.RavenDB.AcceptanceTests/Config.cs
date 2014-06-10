0708d495ba4bb838cd433184452310ebf3984d82
Itamar Syn-Hershko <itamar@code972.com>
2014-06-10 18:50:50 +0300

    AcceptanceTests to use the new config

--- src/NServiceBus.RavenDB.AcceptanceTests/Config.cs
+++ src/NServiceBus.RavenDB.AcceptanceTests/Config.cs
@@ -1,56 +1,14 @@
 ﻿using NServiceBus.Persistence;
-using Raven.Client;
+using NServiceBus.RavenDB;
 using Raven.Client.Embedded;
 // ReSharper disable UnusedParameter.Global
 using NServiceBus;
 
-public abstract class ConfigurePersistences
-{
-    protected readonly IDocumentStore documentStore;
-
-    protected ConfigurePersistences()
-    {
-        documentStore = new EmbeddableDocumentStore
-                        {
-                            RunInMemory = true,
-//#if DEBUG
-//                            UseEmbeddedHttpServer = true, // enable debugging through HTTP
-//#endif
-                        };
-        documentStore.Initialize();
-    }
-}
-
-public class ConfigureRavenDBPersistence : ConfigurePersistences
+public class ConfigureRavenDBPersistence
 {
     public void Configure(Configure config)
     {
-        config.Settings.Set(RavenDbSettingsExtenstions.DocumentStoreSettingsKey, documentStore);
+        config.UsePersistence<RavenDB>(c => c.SetDefaultDocumentStore(new ConnectionParameters { Url = "http://localhost:8080"}));
         // TODO register documentStore to be disposed through NSB Pipeline
     }
 }
-
-// TODO these currently don't get picked up because recent config API changes broke ConfigureExtensions.DefinePersistence
-//public class ConfigureRavenDBTimeoutPersistence : ConfigurePersistences
-//{
-//    public void Configure(Configure config)
-//    {
-//        config.Settings.Set(RavenDbTimeoutSettingsExtenstions.SettingsKey, documentStore);
-//    }
-//}
-//
-//public class ConfigureRavenDBSubscriptionPersistence : ConfigurePersistences
-//{
-//    public void Configure(Configure config)
-//    {
-//        config.Settings.Set(RavenDbSubscriptionSettingsExtenstions.SettingsKey, documentStore);
-//    }
-//}
-//
-//public class ConfigureRavenDBSagaPersister : ConfigurePersistences
-//{
-//    public void Configure(Configure config)
-//    {
-//        config.Settings.Set(RavenDbSagaSettingsExtenstions.SettingsKey, documentStore);
-//    }
-//}
