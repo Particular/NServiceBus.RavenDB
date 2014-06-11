17df5729d60c3f46cf23801739cc160de68756b4
Itamar Syn-Hershko <itamar@code972.com>
2014-06-12 02:38:21 +0300

    Cleanup

--- src/NServiceBus.RavenDB/ConfigureRavenPersistence.cs
+++ src/NServiceBus.RavenDB/ConfigureRavenPersistence.cs
@@ -1,5 +1,8 @@
 namespace NServiceBus.RavenDB
 {
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
     using NServiceBus;
     using Persistence;
     using Raven.Client.Document;
@@ -14,7 +17,7 @@ namespace NServiceBus.RavenDB
         /// </summary>
         static Configure ApplyRavenDBConventions(this Configure config, DocumentStore documentStore)
         {
-            documentStore.ResourceManagerId = RavenPersistenceConstants.DefaultResourceManagerId(config);
+            documentStore.ResourceManagerId = DefaultResourceManagerId(config);
 
 
             if (config.Settings.Get<bool>("Transactions.SuppressDistributedTransactions"))
@@ -23,6 +26,25 @@ namespace NServiceBus.RavenDB
             }
 
             return config;
+        }
+
+        static Guid DefaultResourceManagerId(Configure config)
+        {
+            var resourceManagerId = Address.Local + "-" + "foo";// TODO Configure.DefineEndpointVersionRetriever();
+
+            return DeterministicGuidBuilder(resourceManagerId);
+        }
+
+        static Guid DeterministicGuidBuilder(string input)
+        {
+            // use MD5 hash to get a 16-byte hash of the string
+            using (var provider = new MD5CryptoServiceProvider())
+            {
+                var inputBytes = Encoding.Default.GetBytes(input);
+                var hashBytes = provider.ComputeHash(inputBytes);
+                // generate a guid from the hash:
+                return new Guid(hashBytes);
+            }
         }
     }
 }
