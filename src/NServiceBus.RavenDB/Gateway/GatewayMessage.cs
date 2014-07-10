ee623ae87d62c8c8788e9f922ddeb9ca5056bf29
Simon Cropp <simon.cropp@gmail.com>
2014-07-10 11:04:52 +1000

    add doco for gateway message

--- src/NServiceBus.RavenDB/Gateway/GatewayMessage.cs
+++ src/NServiceBus.RavenDB/Gateway/GatewayMessage.cs
@@ -2,9 +2,18 @@
 {
     using System;
 
+    /// <summary>
+    /// The Gateway message
+    /// </summary>
     public class GatewayMessage
     {
+        /// <summary>
+        /// Id of this message.
+        /// </summary>
         public string Id { get; set; }
+        /// <summary>
+        /// The time at which the message was received.
+        /// </summary>
         public DateTime TimeReceived { get; set; }
     }
 }
