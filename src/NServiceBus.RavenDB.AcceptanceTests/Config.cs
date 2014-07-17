f20df245f53e7be0102dfa1a166c951efc648373
Itamar Syn-Hershko <itamar@code972.com>
2014-07-17 19:51:33 +0300

    Munin is officially unsupported and unreliable for testing

--- src/NServiceBus.RavenDB.AcceptanceTests/Config.cs
+++ src/NServiceBus.RavenDB.AcceptanceTests/Config.cs
@@ -1,14 +1,16 @@
-﻿using NServiceBus;
+﻿using System;
+using NServiceBus;
 using NServiceBus.Persistence;
-using Raven.Client.Embedded;
+using Raven.Client.Document;
 
 public class ConfigureRavenDBPersistence
 {
     public void Configure(Configure config)
     {
-        var store = new EmbeddableDocumentStore
+        var store = new DocumentStore
         {
-            RunInMemory = true
+            Url = "http://localhost:8080",
+            DefaultDatabase = Guid.NewGuid().ToString(),
         };
 
         store.Initialize();
