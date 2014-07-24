de9b635d27bd05eeea8b769743ad7ed673f7f683
John Simons <john.simons@nservicebus.com>
2014-07-24 16:47:45 +1000

    Added tests

---
+++ src/NServiceBus.RavenDB.Tests/SagaPersister/When_persisting_a_saga_entity_with_inherited_property.cs
@@ -0,0 +1,20 @@
+namespace NServiceBus.Core.Tests.Persistence.RavenDB.SagaPersister
+{
+    using NUnit.Framework;
+
+    class When_persisting_a_saga_entity_with_inherited_property : Persisting_a_saga_entity_with_a_raven_saga_persister
+    {
+        public override void SetupEntity(TestSaga saga)
+        {
+            entity.PolymorphicRelatedProperty = new PolymorphicProperty {SomeInt = 9};
+        }
+
+        [Test]
+        public void Inherited_property_classes_should_be_persisted()
+        {
+            var polymorphicRelatedProperty = (PolymorphicProperty)savedEntity.PolymorphicRelatedProperty;
+            var polymorphicProperty = (PolymorphicProperty)entity.PolymorphicRelatedProperty;
+            Assert.AreEqual(polymorphicProperty.SomeInt, polymorphicRelatedProperty.SomeInt);
+        }
+    }
+}
---
+++ src/NServiceBus.RavenDB.Tests/SagaPersister/When_persisting_a_saga_with_the_same_unique_property_as_another_saga.cs
@@ -0,0 +1,30 @@
+namespace NServiceBus.Core.Tests.Persistence.RavenDB.SagaPersister
+{
+    using System;
+    using NServiceBus.Persistence;
+    using NUnit.Framework;
+
+    class When_persisting_a_saga_with_the_same_unique_property_as_another_saga : Raven_saga_persistence_concern
+    {
+        [Test]
+        public void It_should_enforce_uniqueness()
+        {
+            var uniqueString = Guid.NewGuid().ToString();
+
+            var saga1 = new SagaWithUniqueProperty
+                        {
+                            Id = Guid.NewGuid(),
+                            UniqueString = uniqueString
+                        };
+            
+            var saga2 = new SagaWithUniqueProperty
+                        {
+                            Id = Guid.NewGuid(),
+                            UniqueString = uniqueString
+                        };
+
+            SaveSaga(saga1);
+            Assert.Throws<ConcurrencyException>(() => SaveSaga(saga2));
+        }
+    }
+}
