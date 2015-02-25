6953d724264bc7951cc8f8804512ef88366645b2
danielmarbach <daniel.marbach@particular.net>
2015-02-25 11:34:40 +0100

    Adding automatic database deletion based on new cleanup feature in ATT
    Currently checking in the ConfigureExtensions and also the AcceptanceTests.dll because the ATT are already bound to V6

--- src/NServiceBus.RavenDB.AcceptanceTests/Config.cs
+++ src/NServiceBus.RavenDB.AcceptanceTests/Config.cs
@@ -5,16 +5,28 @@ using Raven.Client.Document;
 
 public class ConfigureRavenDBPersistence
 {
+    DocumentStore documentStore;
+
     public void Configure(BusConfiguration config)
     {
-        var store = new DocumentStore
+        documentStore = new DocumentStore
         {
             Url = "http://localhost:8081",
             DefaultDatabase = Guid.NewGuid().ToString(),
+
         };
 
-        store.Initialize();
+        documentStore.Initialize();
 
-        config.UsePersistence<RavenDBPersistence>().DoNotSetupDatabasePermissions().SetDefaultDocumentStore(store);
+        config.UsePersistence<RavenDBPersistence>().DoNotSetupDatabasePermissions().SetDefaultDocumentStore(documentStore);
+    }
+
+    public void Cleanup()
+    {
+        var client = documentStore.AsyncDatabaseCommands.ForSystemDatabase();
+
+        var deleteUrl = string.Format("/admin/databases/{0}?hard-delete=true", Uri.EscapeDataString(documentStore.DefaultDatabase));
+
+        client.CreateRequest(deleteUrl, "DELETE").ExecuteRequestAsync().Wait();
     }
 }
