da5b2a7decc5819b5c06965aa26e07b0d52a10ee
danielmarbach <daniel.marbach@particular.net>
2015-04-21 11:28:10 +0200

    Added Outbox settings extensions which currently extend BusConfiguration

---
+++ src/NServiceBus.RavenDB/Outbox/RavenDBOutboxExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace NServiceBus.RavenDB.Outbox
+{
+    using System;
+    using NServiceBus.Configuration.AdvanceExtensibility;
+
+    /// <summary>
+    /// Contains extensions methods which allow to configure RavenDB outbox specific configuration
+    /// </summary>
+    public static class RavenDBOutboxExtensions
+    {
+        /// <summary>
+        /// Sets the time to keep the deduplication data to the specified time span.
+        /// </summary>
+        /// <param name="configuration">The configuration being extended</param>
+        /// <param name="timeToKeepDeduplicationData"></param>
+        /// <returns>The configuration</returns>
+        public static BusConfiguration SetTimeToKeepDeduplicationData(this BusConfiguration configuration, TimeSpan timeToKeepDeduplicationData)
+        {
+            configuration.GetSettings().Set("Outbox.TimeToKeepDeduplicationData", timeToKeepDeduplicationData);
+            return configuration;
+        }
+
+        /// <summary>
+        /// Sets the frequency to run the deduplication data cleanup task.
+        /// </summary>
+        /// <param name="configuration">The configuration being extended</param>
+        /// <param name="frequencyToRunDeduplicationDataCleanup">The frequency to run the deduplication data cleanup task. By specifying a negative time span (-1) the cleanup task will never run.</param>
+        /// <returns>The configuration</returns>
+        public static BusConfiguration SetFrequencyToRunDeduplicationDataCleanup(this BusConfiguration configuration, TimeSpan frequencyToRunDeduplicationDataCleanup)
+        {
+            configuration.GetSettings().Set("Outbox.FrequencyToRunDeduplicationDataCleanup", frequencyToRunDeduplicationDataCleanup);
+            return configuration;
+        }
+    }
+}
