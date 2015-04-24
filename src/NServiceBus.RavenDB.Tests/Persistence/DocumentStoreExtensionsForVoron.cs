72d70f58fc37afa08e3ce5dca291e8d246a8021f
danielmarbach <daniel.marbach@particular.net>
2015-04-24 16:48:56 +0200

    Verify that users don't enable DTC on top of a database configured to use Voron

---
+++ src/NServiceBus.RavenDB.Tests/Persistence/DocumentStoreExtensionsForVoron.cs
@@ -0,0 +1,41 @@
+﻿namespace NServiceBus.RavenDB.Tests.Persistence
+{
+    using System;
+    using System.Collections.Generic;
+    using Raven.Abstractions.Data;
+    using Raven.Client.Document;
+
+    internal static class DocumentStoreExtensionsForVoron
+    {
+        public static IDisposable SetupVoronTest(this DocumentStore store)
+        {
+            store.Initialize();
+            store.DatabaseCommands.GlobalAdmin.CreateDatabase(new DatabaseDocument
+            {
+                Id = "VoronTest",
+                Settings = new Dictionary<string, string>
+                    {
+                        { "Raven/StorageTypeName", "voron" },
+                        { "Raven/DataDir", "~/VoronTest" }
+                    }
+            });
+            store.DefaultDatabase = "VoronTest";
+            return new VoronTestDeleter(store);
+        }
+
+        class VoronTestDeleter : IDisposable
+        {
+            readonly DocumentStore store;
+
+            public VoronTestDeleter(DocumentStore store)
+            {
+                this.store = store;
+            }
+
+            public void Dispose()
+            {
+                store.DatabaseCommands.GlobalAdmin.DeleteDatabase("VoronTest", hardDelete: true);
+            }
+        }
+    }
+}
---
+++ src/NServiceBus.RavenDB/Internal/StorageEngineVerifier.cs
@@ -0,0 +1,26 @@
+﻿namespace NServiceBus.RavenDB.Internal
+{
+    using System;
+    using NServiceBus.Settings;
+    using Raven.Client;
+
+    class StorageEngineVerifier
+    {
+        const string StorageEngineDoesntSupportDtcMessage =
+            @"The selected database is using a storage engine which doesn't support DTC. Either choose another storage engine or disable distributed transactions in the transaction settings";
+
+        internal static void VerifyStorageEngineSupportsDtcIfRequired(IDocumentStore store, ReadOnlySettings settings)
+        {
+            if (settings.Get<bool>("Transactions.SuppressDistributedTransactions"))
+            {
+                return;
+            }
+
+            var stats = store.DatabaseCommands.GetStatistics();
+            if (!stats.SupportsDtc)
+            {
+                throw new InvalidOperationException(StorageEngineDoesntSupportDtcMessage);
+            }
+        }
+    }
+}
