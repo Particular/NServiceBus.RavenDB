0e4580bea1b85df9171e7eb3ccbb07f73b997b8f
Simon Cropp <simon.cropp@gmail.com>
2015-09-18 09:32:53 +1000

    typos

--- src/NServiceBus.RavenDB/Internal/RavenBuildInfo.cs
+++ src/NServiceBus.RavenDB/Internal/RavenBuildInfo.cs
@@ -17,16 +17,16 @@
             }
             if (productVersion.Revision > 0)
             {
-                throw new Exception(string.Format("Could not convert RavenDB server product version '{0}' conained a revision which was unexpected.", ProductVersion));
+                throw new Exception(string.Format("Could not convert RavenDB server product version '{0}' contained a revision which was unexpected.", ProductVersion));
             }
             if (productVersion.Build > 0)
             {
-                throw new Exception(string.Format("Could not convert RavenDB server product version '{0}' conained a build which was unexpected.", ProductVersion));
+                throw new Exception(string.Format("Could not convert RavenDB server product version '{0}' contained a build which was unexpected.", ProductVersion));
             }
             int buildVersion;
             if (!int.TryParse(BuildVersion, out buildVersion))
             {
-                throw new Exception(string.Format("Could not convert RavenDB server build version '{0}' to a int.", BuildVersion));
+                throw new Exception(string.Format("Could not convert RavenDB server build version '{0}' to an int.", BuildVersion));
             }
             return new Version(productVersion.Major,productVersion.Minor,buildVersion);
         }
