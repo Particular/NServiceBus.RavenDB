683b816cbf723839e780fe7359b2c21b2c66217b
andreasohlund <andreas.ohlund@particular.net>
2015-10-22 13:52:19 +0200

    Supressing TX to rule that out

--- src/NServiceBus.RavenDB/Subscriptions/SubscriptionPersister.cs
+++ src/NServiceBus.RavenDB/Subscriptions/SubscriptionPersister.cs
@@ -3,9 +3,11 @@ namespace NServiceBus.Unicast.Subscriptions.RavenDB
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using System.Transactions;
     using NServiceBus.Extensibility;
     using NServiceBus.RavenDB.Persistence.SubscriptionStorage;
     using NServiceBus.Unicast.Subscriptions.MessageDrivenSubscriptions;
+    using Raven.Abstractions.Exceptions;
     using Raven.Client;
 
     class SubscriptionPersister : ISubscriptionStorage
@@ -19,43 +21,46 @@ namespace NServiceBus.Unicast.Subscriptions.RavenDB
         {
             var messageTypeLookup = messageTypes.ToDictionary(Subscription.FormatId);
 
-            //var attempts = 0;
+            var attempts = 0;
 
             //note: since we have a design that can run into concurrency exceptions we perform a few retries
             // we should redesign this in the future to use a separate doc per subscriber and message type
-            //do
-            //{
-            //try
-            //{
-            using (var session = OpenSession())
+            using (new TransactionScope(TransactionScopeOption.Suppress, TransactionScopeAsyncFlowOption.Enabled))
             {
-                session.Advanced.UseOptimisticConcurrency = true;
-
-                var existingSubscriptions = GetSubscriptions(messageTypeLookup.Values, session).ToLookup(m => m.Id);
-
-                var newAndExistingSubscriptions = messageTypeLookup
-                    .Select(id => existingSubscriptions[id.Key].SingleOrDefault() ?? StoreNewSubscription(session, id.Key, id.Value))
-                    .Where(subscription => subscription.Clients.All(c => c != client)).ToArray();
-
-                foreach (var subscription in newAndExistingSubscriptions)
+                do
                 {
-                    subscription.Clients.Add(client);
-                }
+                    try
+                    {
+                        using (var session = OpenSession())
+                        {
+                            session.Advanced.UseOptimisticConcurrency = true;
 
-                session.SaveChanges();
+                            var existingSubscriptions = GetSubscriptions(messageTypeLookup.Values, session).ToLookup(m => m.Id);
+
+                            var newAndExistingSubscriptions = messageTypeLookup
+                                .Select(id => existingSubscriptions[id.Key].SingleOrDefault() ?? StoreNewSubscription(session, id.Key, id.Value))
+                                .Where(subscription => subscription.Clients.All(c => c != client)).ToArray();
+
+                            foreach (var subscription in newAndExistingSubscriptions)
+                            {
+                                subscription.Clients.Add(client);
+                            }
+
+                            session.SaveChanges();
+                        }
+
+                        return Task.FromResult(0);
+                    }
+                    catch (ConcurrencyException)
+                    {
+                        attempts++;
+                    }
+                }
+                while (attempts < 5);
             }
 
+
             return Task.FromResult(0);
-            //}
-            //catch (ConcurrencyException)
-            //{
-            //    attempts++;
-            //} 
-            //}
-            //while (attempts < 5);
-
-
-            //return Task.FromResult(0);
         }
 
         public Task Unsubscribe(string client, IEnumerable<MessageType> messageTypes, ContextBag context)
