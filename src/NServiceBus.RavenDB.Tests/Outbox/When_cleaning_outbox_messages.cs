bbf6de8f956cec872495a63a576b424a24eb7fbc
Mauro Servienti <mauro@topics.it>
2015-11-25 17:08:06 +0100

    Outbox support for async/await

--- src/NServiceBus.RavenDB.Tests/Outbox/When_cleaning_outbox_messages.cs
+++ src/NServiceBus.RavenDB.Tests/Outbox/When_cleaning_outbox_messages.cs
@@ -9,6 +9,7 @@
     using NServiceBus.Outbox;
     using NServiceBus.RavenDB.Outbox;
     using NUnit.Framework;
+    using Raven.Client;
 
     [TestFixture]
     public class When_cleaning_outbox_messages : RavenDBPersistenceTestBase
@@ -30,7 +31,7 @@
 
             var persister = new OutboxPersister(store);
 
-            using (var transaction = await persister.BeginTransaction(context))
+            using(var transaction = await persister.BeginTransaction(context))
             {
                 await persister.Store(new OutboxMessage("NotDispatched", new List<TransportOperation>()), transaction, context);
 
@@ -38,12 +39,12 @@
             }
 
             var outboxMessage = new OutboxMessage(id, new List<TransportOperation>
-            {
-                new TransportOperation(id, new Dictionary<string, string>(), new byte[1024*5], new Dictionary<string, string>())
-            });
+                {
+                    new TransportOperation(id, new Dictionary<string, string>(), new byte[1024*5], new Dictionary<string, string>())
+                });
 
 
-            using (var transaction = await persister.BeginTransaction(context))
+            using(var transaction = await persister.BeginTransaction(context))
             {
                 await persister.Store(outboxMessage, transaction, context);
                 await transaction.Commit();
@@ -53,17 +54,18 @@
             await persister.SetAsDispatched(id, context);
             Thread.Sleep(TimeSpan.FromSeconds(1)); //Need to wait for dispatch logic to finish
 
+            WaitForUserToContinueTheTest(store);
             WaitForIndexing(store);
 
             var cleaner = new OutboxRecordsCleaner
             {
                 DocumentStore = store
             };
-            cleaner.RemoveEntriesOlderThan(DateTime.UtcNow.AddMinutes(1));
+            await cleaner.RemoveEntriesOlderThan(DateTime.UtcNow.AddMinutes(1));
 
-            using (var s = store.OpenSession())
+            using(var s = store.OpenAsyncSession())
             {
-                var result = s.Query<OutboxRecord>().ToList();
+                var result = await s.Query<OutboxRecord>().ToListAsync();
 
                 Assert.AreEqual(1, result.Count);
                 Assert.AreEqual("NotDispatched", result[0].MessageId);
