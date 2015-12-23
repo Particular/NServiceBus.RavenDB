296f976385e99734f739bfa1aeafc303f350808b
Mauro Servienti <mauro@topics.it>
2015-12-23 15:51:04 +0100

    raven session message context extension

---
+++ src/NServiceBus.RavenDB/SessionManagement/RavenSessionExtension.cs
@@ -0,0 +1,20 @@
+﻿using Raven.Client;
+
+namespace NServiceBus.RavenDB.Persistence
+{
+    /// <summary>
+    /// Extensions, to the message handler context, to manage RavenDB session.
+    /// </summary>
+    public static class RavenSessionExtension
+    {
+        /// <summary>
+        /// Gets the current RavenDB session.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns></returns>
+        public static IAsyncDocumentSession GetRavenSession(this IMessageHandlerContext context)
+        {
+            return context.Extensions.Get<IAsyncDocumentSession>();
+        }
+    }
+}
