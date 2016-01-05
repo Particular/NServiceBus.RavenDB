26875ac650c80346ab4250e3423ffc8d95327598
Colin Higgins <colin.higgins@particular.net>
2016-01-05 14:20:25 -0500

    Test for accessing raven session with saga

---
+++ src/NServiceBus.RavenDB.AcceptanceTests/When_accessing_raven_session_from_handler.cs
@@ -0,0 +1,77 @@
+﻿namespace NServiceBus.AcceptanceTests.ApiExtension
+{
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NServiceBus.RavenDB.Persistence;
+    using NUnit.Framework;
+    using Raven.Client;
+
+    public class When_accessing_raven_session_from_handler : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task It_should_return_configured_session()
+        {
+            var context =
+                await Scenario.Define<RavenSessionTestContext>()
+                    .WithEndpoint<RavenSessionExtensions>(b => b.When((bus, c) =>
+                    {
+                        var options = new SendOptions();
+
+                        options.RouteToLocalEndpointInstance();
+
+                        return bus.Send(new RavenSessionExtensions.GenericMessage(), options);
+                    }))
+                    .Done(c => c.HandlerWasHit)
+                    .Run();
+
+            Assert.IsNotNull(context.RavenSessionFromHandler);
+        }
+
+        public class RavenSessionTestContext : ScenarioContext
+        {
+            public IAsyncDocumentSession RavenSessionFromTest { get; set; }
+            public IAsyncDocumentSession RavenSessionFromHandler { get; set; }
+            public bool HandlerWasHit { get; set; }
+        }
+
+        public class RavenSessionExtensions : EndpointConfigurationBuilder
+        {
+            public RavenSessionExtensions()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            public class SagaData : IContainSagaData
+            {
+                public Guid Id { get; set; }
+                public string Originator { get; set; }
+                public string OriginalMessageId { get; set; }
+            }
+
+            public class GenericSaga : Saga<SagaData>, IAmStartedByMessages<GenericMessage>
+            {
+                public RavenSessionTestContext TestContext { get; set; }
+
+                public Task Handle(GenericMessage message, IMessageHandlerContext context)
+                {
+                    TestContext.RavenSessionFromHandler = context.GetRavenSession();
+                    TestContext.HandlerWasHit = true;
+                    return Task.FromResult(0);
+                }
+
+                protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaData> mapper)
+                {
+                    mapper.ConfigureMapping<GenericMessage>(m => m.Id).ToSaga(s => s.Id);
+                }
+            }
+
+            [Serializable]
+            public class GenericMessage : IMessage
+            {
+                public Guid Id { get; set; }
+            }
+        }
+    }
+}
