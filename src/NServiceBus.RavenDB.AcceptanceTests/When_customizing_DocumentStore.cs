b3264080a897d88a5c206553fc8a8b9469cc819f
David Boike <david.boike@gmail.com>
2016-01-27 13:24:58 -0600

    Centralizing DocumentStore managment

---
+++ src/NServiceBus.RavenDB.AcceptanceTests/When_customizing_DocumentStore.cs
@@ -0,0 +1,148 @@
+﻿namespace NServiceBus.AcceptanceTests.Sagas
+{
+    using System;
+    using EndpointTemplates;
+    using AcceptanceTesting;
+    using NServiceBus.Configuration.AdvanceExtensibility;
+    using NServiceBus.RavenDB.Persistence;
+    using NServiceBus.Saga;
+    using NUnit.Framework;
+    using Raven.Client.Document;
+    using Raven.Client.Document.DTC;
+
+    public class When_customzing_DocStore : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_honor_customized_values()
+        {
+            var context = new Context();
+            context.ExpectedResourceManagerId = Guid.NewGuid();
+
+            Scenario.Define(context)
+                .WithEndpoint<CustomizeDocStoreEndpoint>(b =>
+                {
+                    b.Given(bus => bus.SendLocal(new TestCmd { Name = "Doesn't matter, let's say Fred"}));
+
+                    b.CustomConfig(cfg =>
+                    {
+                        ConfigureRavenDBPersistence.GetDefaultPersistenceExtensions(cfg.GetSettings())
+                        .CustomizeDocumentStore(store =>
+                        {
+                            var ds = store as DocumentStore;
+                            ds.Identifier = "TestIdentifier";
+                            ds.ResourceManagerId = context.ExpectedResourceManagerId;
+                            ds.TransactionRecoveryStorage = new VolatileOnlyTransactionRecoveryStorage();
+                        });
+                    });
+                })
+                .Done(c => c.MessageReceived)
+                .Run();
+            
+            Assert.IsNotNull(context.DocStore);
+            Assert.AreEqual("TestIdentifier", context.DocStore.Identifier);
+            Assert.AreEqual(context.ExpectedResourceManagerId, context.DocStore.ResourceManagerId);
+            Assert.IsInstanceOf<VolatileOnlyTransactionRecoveryStorage>(context.DocStore.TransactionRecoveryStorage);
+        }
+
+        [Test]
+        public void Should_not_change_tx_recovery_values_for_provided_DocStore_with_IsolatedStorage()
+        {
+            var context = new Context();
+
+            context.ExpectedResourceManagerId = Guid.NewGuid();
+
+            Scenario.Define(context)
+                .WithEndpoint<CustomizeDocStoreEndpoint>(b =>
+                {
+                    b.Given(bus => bus.SendLocal(new TestCmd { Name = "Doesn't matter, let's say George" }));
+
+                    b.CustomConfig(cfg =>
+                    {
+                        var ds = ConfigureRavenDBPersistence.GetDefaultDocumentStore(cfg.GetSettings()) as DocumentStore;
+                        ds.ResourceManagerId = context.ExpectedResourceManagerId;
+                        ds.TransactionRecoveryStorage = new IsolatedStorageTransactionRecoveryStorage();
+                    });
+                })
+                .Done(c => c.MessageReceived)
+                .Run();
+
+            Assert.IsNotNull(context.DocStore);
+            Assert.AreEqual(context.ExpectedResourceManagerId, context.DocStore.ResourceManagerId);
+            Assert.IsInstanceOf<IsolatedStorageTransactionRecoveryStorage>(context.DocStore.TransactionRecoveryStorage);
+        }
+
+        [Test]
+        public void Should_not_change_tx_recovery_values_for_provided_DocStore_with_LocalDirectoryStorage()
+        {
+            var context = new Context();
+
+            context.ExpectedResourceManagerId = Guid.NewGuid();
+
+            Scenario.Define(context)
+                .WithEndpoint<CustomizeDocStoreEndpoint>(b =>
+                {
+                    b.Given(bus => bus.SendLocal(new TestCmd { Name = "Doesn't matter, let's say George" }));
+
+                    b.CustomConfig(cfg =>
+                    {
+                        var ds = ConfigureRavenDBPersistence.GetDefaultDocumentStore(cfg.GetSettings()) as DocumentStore;
+                        ds.ResourceManagerId = context.ExpectedResourceManagerId;
+                        // ConfigureRavenDBPersistence already sets LocalDirectoryTransactionRecoveryStorage
+                    });
+                })
+                .Done(c => c.MessageReceived)
+                .Run();
+
+            Assert.IsNotNull(context.DocStore);
+            Assert.AreEqual(context.ExpectedResourceManagerId, context.DocStore.ResourceManagerId);
+            Assert.IsInstanceOf<LocalDirectoryTransactionRecoveryStorage>(context.DocStore.TransactionRecoveryStorage);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool MessageReceived { get; set; }
+            public Guid ExpectedResourceManagerId { get; set; }
+            public DocumentStore DocStore { get; set; }
+        }
+
+        public class CustomizeDocStoreEndpoint : EndpointConfigurationBuilder
+        {
+            public CustomizeDocStoreEndpoint()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            public class TestCustomizeDocStoreSaga : Saga<CustomizeDocStoreSagaData>,
+                IAmStartedByMessages<TestCmd>
+            {
+                public Context Context { get; set; }
+                public ISessionProvider SessionProvider { get; set; }
+
+                public void Handle(TestCmd message)
+                {
+                    this.Data.Name = message.Name;
+
+                    Context.DocStore = SessionProvider.Session.Advanced.DocumentStore as DocumentStore;
+                    Context.MessageReceived = true;
+                }
+
+                protected override void ConfigureHowToFindSaga(SagaPropertyMapper<CustomizeDocStoreSagaData> mapper)
+                {
+                    mapper.ConfigureMapping<TestCmd>(msg => msg.Name).ToSaga(saga => saga.Name);
+                }
+            }
+
+            public class CustomizeDocStoreSagaData : ContainSagaData
+            {
+                [Unique]
+                public string Name { get; set; }
+            }
+        }
+
+        public class TestCmd : ICommand
+        {
+            public string Name { get; set; }
+        }
+    }
+
+}
--- src/NServiceBus.RavenDB/Internal/SharedDocumentStore.cs
+++ src/NServiceBus.RavenDB/Internal/SharedDocumentStore.cs
@@ -2,57 +2,17 @@
 {
     using NServiceBus.Features;
     using NServiceBus.RavenDB.Internal;
-    using NServiceBus.Settings;
-    using Raven.Client;
-    using Raven.Client.Document;
 
     class SharedDocumentStore : Feature
     {
         public SharedDocumentStore()
         {
-            Defaults(_ => _.Set<DocumentStoreHolder>(new DocumentStoreHolder()));
+            Defaults(_ => _.Set<SingleSharedDocumentStore>(new SingleSharedDocumentStore()));
         }
 
         protected override void Setup(FeatureConfigurationContext context)
         {
             // no-op, the docstore is initialized lazily
-        }
-
-        public static IDocumentStore Get(ReadOnlySettings settings)
-        {
-            // The holder is known to be non-null since we set it in the ctor
-            var holder = settings.Get<DocumentStoreHolder>();
-            if (holder.DocumentStore == null)
-            {
-                var p = settings.GetOrDefault<ConnectionParameters>(RavenDbSettingsExtensions.DefaultConnectionParameters);
-                if (p != null)
-                {
-                    holder.DocumentStore = new DocumentStore
-                    {
-                        Url = p.Url,
-                        DefaultDatabase = p.DatabaseName ?? settings.EndpointName(),
-                        ApiKey = p.ApiKey,
-                        Credentials = p.Credentials
-                    };
-                    Helpers.ApplyRavenDBConventions(settings, holder.DocumentStore);
-                    holder.DocumentStore.Initialize();
-                }
-                else
-                {
-                    holder.DocumentStore = Helpers.CreateDocumentStoreByConnectionStringName(settings, "NServiceBus/Persistence/RavenDB", "NServiceBus/Persistence");
-
-                    if (holder.DocumentStore == null)
-                    {
-                        holder.DocumentStore = Helpers.CreateDocumentStoreByUrl(settings, "http://localhost:8080");
-                    }
-                }
-            }
-            return holder.DocumentStore;
-        }
-
-        class DocumentStoreHolder
-        {
-            public IDocumentStore DocumentStore { get; set; }
         }
     }
 }
