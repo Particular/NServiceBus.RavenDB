d2a6793b136afa6fb4f94f0c8e3d953b4774934a
David Boike <david.boike@gmail.com>
2016-01-27 13:24:58 -0600

    Shared, centralized method of finding/creating DocumentStores and setting tx settings

--- src/NServiceBus.RavenDB.Tests/RavenDBPersistenceTestBase.cs
+++ src/NServiceBus.RavenDB.Tests/RavenDBPersistenceTestBase.cs
@@ -1,5 +1,6 @@
 ﻿namespace NServiceBus.RavenDB.Tests
 {
+    using System;
     using NUnit.Framework;
     using Raven.Client;
     using Raven.Tests.Helpers;
@@ -11,6 +12,7 @@
         [SetUp]
         public virtual void SetUp()
         {
+            Console.WriteLine($"Execution Environment: 64-bit OS:{Environment.Is64BitOperatingSystem}, 64-bit Process:{Environment.Is64BitProcess}");
             store = NewDocumentStore();
         }
 
