64cecb1dc83b60732920ea58ff5eb7dc6f4db0fb
Tim Bussmann <timbussmann@users.noreply.github.com>
2016-04-07 16:50:22 +0200

    added TestableRavenStorageSession

---
+++ src/NServiceBus.RavenDB/Testing/TestableRavenStorageSession.cs
@@ -0,0 +1,25 @@
+﻿namespace NServiceBus.Testing
+{
+    using Persistence;
+    using Raven.Client;
+
+    /// <summary>
+    /// A fake implementation for <see cref="SynchronizedStorageSession"/> for testing purposes.
+    /// </summary>
+    public class TestableRavenStorageSession : SynchronizedStorageSession
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="TestableRavenStorageSession"/> using the provided <see cref="IAsyncDocumentSession"/>.
+        /// </summary>
+        /// <param name="session"></param>
+        public TestableRavenStorageSession(IAsyncDocumentSession session)
+        {
+            Session = session;
+        }
+
+        /// <summary>
+        /// The document session which is retrieved by calling <see cref="RavenSessionExtension.RavenSession"/>.
+        /// </summary>
+        public IAsyncDocumentSession Session { get; private set; }
+    }
+}
