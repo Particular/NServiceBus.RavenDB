adf5ed7f2f23dd30cc4060b4fecd6abec4e75bb8
Colin Higgins <colin.higgins@particular.net>
2016-04-11 19:12:48 -0400

    Remove xml doco

--- src/NServiceBus.RavenDB/SagaPersister/RavenDbSagaStorage.cs
+++ src/NServiceBus.RavenDB/SagaPersister/RavenDbSagaStorage.cs
@@ -2,22 +2,13 @@
 {
     using NServiceBus.SagaPersisters.RavenDB;
 
-    /// <summary>
-    ///     RavenDB Saga Storage.
-    /// </summary>
     class RavenDbSagaStorage : Feature
     {
-        /// <summary>
-        ///     Creates an instance of <see cref="RavenDbSagaStorage" />.
-        /// </summary>
         internal RavenDbSagaStorage()
         {
             DependsOn<Sagas>();
         }
 
-        /// <summary>
-        ///     Called when the feature should perform its initialization. This call will only happen if the feature is enabled.
-        /// </summary>
         protected override void Setup(FeatureConfigurationContext context)
         {
             context.Container.ConfigureComponent<SagaPersister>(DependencyLifecycle.InstancePerCall);
