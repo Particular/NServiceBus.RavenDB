02d5ae3cbdf74cc173185631eb697815c68a9979
David Boike <david.boike@gmail.com>
2016-04-11 15:15:31 -0600

    Adding APIApprover (#194)

---
+++ src/NServiceBus.RavenDB.Tests/API/APIApprovals.cs
@@ -0,0 +1,26 @@
+﻿namespace NServiceBus.RavenDB.Tests.API
+{
+    using System.IO;
+    using System.Runtime.CompilerServices;
+    using ApiApprover;
+    using ApprovalTests;
+    using ApprovalTests.Reporters;
+    using Mono.Cecil;
+    using NUnit.Framework;
+
+    [TestFixture]
+    class APIApprovals
+    {
+        [Test]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        [UseReporter(typeof(DiffReporter))]
+        public void ApproveRavenDbPersistence()
+        {
+            var assemblyPath = Path.GetFullPath(typeof(RavenDBPersistence).Assembly.Location);
+            var asm = AssemblyDefinition.ReadAssembly(assemblyPath);
+            var publicApi = PublicApiGenerator.CreatePublicApiForAssembly(asm, definition => true, false);
+
+            Approvals.Verify(publicApi);
+        }
+    }
+}
