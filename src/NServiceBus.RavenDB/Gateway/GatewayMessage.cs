e73f799df86b3648bdb23a6004a9b7117d282cad
Colin Higgins <colin.higgins@particular.net>
2016-04-11 19:12:57 -0400

    Make GatewayMessage internal

--- src/NServiceBus.RavenDB/Gateway/GatewayMessage.cs
+++ src/NServiceBus.RavenDB/Gateway/GatewayMessage.cs
@@ -2,19 +2,9 @@
 {
     using System;
 
-    /// <summary>
-    ///     The Gateway message
-    /// </summary>
-    public class GatewayMessage
+    class GatewayMessage
     {
-        /// <summary>
-        ///     Id of this message.
-        /// </summary>
         public string Id { get; set; }
-
-        /// <summary>
-        ///     The time at which the message was received.
-        /// </summary>
         public DateTime TimeReceived { get; set; }
     }
 }
