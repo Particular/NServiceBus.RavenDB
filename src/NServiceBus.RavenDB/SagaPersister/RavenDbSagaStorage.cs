2bdf07193cc487e5b310d21571135576e18f2d74
Colin Higgins <colin.higgins@particular.net>
2016-04-18 12:04:13 -0400

    Use ObsoleteEx instead of internalizing in this version

--- src/NServiceBus.RavenDB/SagaPersister/RavenDbSagaStorage.cs
+++ src/NServiceBus.RavenDB/SagaPersister/RavenDbSagaStorage.cs
@@ -2,13 +2,20 @@
 {
     using NServiceBus.SagaPersisters.RavenDB;
 
-    class RavenDbSagaStorage : Feature
+    /// <summary>
+    ///     RavenDB Saga Storage.
+    /// </summary>
+    [ObsoleteEx(Message = "This type was not meant to be used in external code is being made internal.", RemoveInVersion = "5", TreatAsErrorFromVersion = "4")]
+    public class RavenDbSagaStorage : Feature
     {
         internal RavenDbSagaStorage()
         {
             DependsOn<Sagas>();
         }
 
+        /// <summary>
+        ///     Called when the feature should perform its initialization. This call will only happen if the feature is enabled.
+        /// </summary>
         protected override void Setup(FeatureConfigurationContext context)
         {
             context.Container.ConfigureComponent<SagaPersister>(DependencyLifecycle.InstancePerCall);
