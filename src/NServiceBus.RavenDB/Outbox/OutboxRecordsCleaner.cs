4f97c3cab46262255b03d3bc6c5c63055c027d68
David Boike <david.boike@gmail.com>
2016-07-25 15:40:47 -0500

    Use DeleteByIndex in OutboxRecordsCleaner, execute with Task.Delay loop

--- src/NServiceBus.RavenDB/Outbox/OutboxRecordsCleaner.cs
+++ src/NServiceBus.RavenDB/Outbox/OutboxRecordsCleaner.cs
@@ -1,11 +1,8 @@
 ﻿namespace NServiceBus.Persistence.RavenDB
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
-    using NServiceBus.RavenDB.Outbox;
-    using Raven.Abstractions.Commands;
+    using Raven.Abstractions.Data;
     using Raven.Client;
 
     class OutboxRecordsCleaner
@@ -15,61 +12,22 @@
             this.documentStore = documentStore;
         }
 
-        public async Task RemoveEntriesOlderThan(DateTime dateTime)
+        public Task RemoveEntriesOlderThan(DateTime dateTime)
         {
-            lock (this)
+            var query = new IndexQuery
             {
-                if (doingCleanup)
-                {
-                    return;
-                }
+                Query = $"DispatchedAt:[* TO {dateTime:o}]"
+            };
 
-                doingCleanup = true;
-            }
-
-            try
+            var bulkOpts = new BulkOperationOptions
             {
-                var deletionCommands = new List<ICommandData>();
+                AllowStale = true
+            };
 
-                using (var session = documentStore.OpenAsyncSession())
-                {
-                    var query = session.Query<OutboxRecord, OutboxRecordsIndex>()
-                        .Where(o => o.Dispatched)
-                        .OrderBy(o => o.DispatchedAt);
-
-                    using (var enumerator = await session.Advanced.StreamAsync(query).ConfigureAwait(false))
-                    {
-                        while (await enumerator.MoveNextAsync().ConfigureAwait(false))
-                        {
-                            if (enumerator.Current.Document.DispatchedAt >= dateTime)
-                            {
-                                break; // break streaming if we went past the threshold
-                            }
-
-                            /*
-                             * For some unknown reasons when streaming classes with no
-                             * Id property the Key is not filled. The document Id can be
-                             * found in the streamed document metadata.
-                             */
-                            var key = enumerator.Current.Key ?? enumerator.Current.Metadata["@id"].ToString();
-
-                            deletionCommands.Add(new DeleteCommandData
-                            {
-                                Key = key
-                            });
-                        }
-                    }
-                }
-
-                await documentStore.AsyncDatabaseCommands.BatchAsync(deletionCommands).ConfigureAwait(false);
-            }
-            finally
-            {
-                doingCleanup = false;
-            }
+            var operation = documentStore.DatabaseCommands.DeleteByIndex(nameof(OutboxRecordsIndex), query, bulkOpts);
+            return operation.WaitForCompletionAsync();
         }
 
         IDocumentStore documentStore;
-        volatile bool doingCleanup;
     }
 }
--- src/NServiceBus.RavenDB/Outbox/RavenDbOutboxStorage.cs
+++ src/NServiceBus.RavenDB/Outbox/RavenDbOutboxStorage.cs
@@ -4,6 +4,7 @@
     using System.Threading;
     using System.Threading.Tasks;
     using NServiceBus.Features;
+    using NServiceBus.Logging;
     using NServiceBus.Persistence;
     using NServiceBus.Settings;
 
@@ -30,59 +31,79 @@
             context.RegisterStartupTask(builder => builder.Build<OutboxCleaner>());
         }
 
-        class OutboxCleaner : FeatureStartupTask, IDisposable
+        class OutboxCleaner : FeatureStartupTask
         {
             public OutboxCleaner(OutboxRecordsCleaner cleaner, ReadOnlySettings settings)
             {
                 this.cleaner = cleaner;
                 this.settings = settings;
-            }
-
-
-            public void Dispose()
-            {
-                if(cleanupTimer != null)
-                {
-                    cleanupTimer.Dispose();
-                    cleanupTimer = null;
-                }
+                this.logger = LogManager.GetLogger<OutboxCleaner>();
             }
 
             protected override Task OnStart(IMessageSession session)
             {
                 timeToKeepDeduplicationData = settings.GetOrDefault<TimeSpan?>("Outbox.TimeToKeepDeduplicationData") ?? TimeSpan.FromDays(7);
 
-                var frequencyToRunDeduplicationDataCleanup = settings.GetOrDefault<TimeSpan?>("Outbox.FrequencyToRunDeduplicationDataCleanup") ?? TimeSpan.FromMinutes(1);
+                frequencyToRunDeduplicationDataCleanup = settings.GetOrDefault<TimeSpan?>("Outbox.FrequencyToRunDeduplicationDataCleanup") ?? TimeSpan.FromMinutes(1);
 
-                cleanupTimer = new Timer(PerformCleanup, null, TimeSpan.FromMinutes(1), frequencyToRunDeduplicationDataCleanup);
+                cancellationTokenSource = new CancellationTokenSource();
+                cancellationToken = cancellationTokenSource.Token;
+
+                cleanupTask = Task.Run(() => PerformCleanup(), CancellationToken.None);
 
                 return TaskEx.CompletedTask;
             }
 
-            protected override Task OnStop(IMessageSession session)
+            protected override async Task OnStop(IMessageSession session)
             {
-                using(var waitHandle = new ManualResetEvent(false))
+                cancellationTokenSource.Cancel();
+
+                // ReSharper disable once MethodSupportsCancellation
+                var timeoutTask = Task.Delay(TimeSpan.FromSeconds(30));
+                var finishedTask = await Task.WhenAny(cleanupTask, timeoutTask).ConfigureAwait(false);
+
+                if (finishedTask == timeoutTask)
                 {
-                    cleanupTimer.Dispose(waitHandle);
-                    waitHandle.WaitOne();
-                    cleanupTimer = null;
+                    logger.Error("RavenOutboxCleaner failed to stop within the time allowed (30s).");
                 }
-
-                return TaskEx.CompletedTask;
             }
 
-            void PerformCleanup(object state)
+            async Task PerformCleanup()
             {
-                cleaner.RemoveEntriesOlderThan(DateTime.UtcNow - timeToKeepDeduplicationData)
-                    .GetAwaiter()
-                    .GetResult();
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    var nextClean = DateTime.UtcNow.Add(frequencyToRunDeduplicationDataCleanup);
+
+                    try
+                    {
+                        var olderThan = DateTime.UtcNow - timeToKeepDeduplicationData;
+                        await cleaner.RemoveEntriesOlderThan(olderThan).ConfigureAwait(false);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        // Graceful shutdown
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Warn("Unable to remove expired Outbox records from Raven database.", ex);
+                    }
+
+                    var delay = nextClean - DateTime.UtcNow;
+                    if (delay > TimeSpan.Zero)
+                    {
+                        await Task.Delay(delay, cancellationToken).ConfigureAwait(false);
+                    }
+                }
             }
 
-
+            Task cleanupTask;
             OutboxRecordsCleaner cleaner;
             ReadOnlySettings settings;
-            Timer cleanupTimer;
             TimeSpan timeToKeepDeduplicationData;
+            TimeSpan frequencyToRunDeduplicationDataCleanup;
+            CancellationTokenSource cancellationTokenSource;
+            CancellationToken cancellationToken;
+            ILog logger;
         }
     }
 }
