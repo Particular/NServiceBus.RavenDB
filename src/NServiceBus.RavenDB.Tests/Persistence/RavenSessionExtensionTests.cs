bb3a7e91a044e298878a42e6b1bd4166fd64ebda
David Boike <david.boike@gmail.com>
2016-07-28 09:33:27 -0500

    Added simple test for RavenSession extension

---
+++ src/NServiceBus.RavenDB.Tests/Persistence/RavenSessionExtensionTests.cs
@@ -0,0 +1,36 @@
+﻿namespace NServiceBus.RavenDB.Tests.Persistence
+{
+    using NServiceBus.Persistence.RavenDB;
+    using NServiceBus.Testing;
+    using NUnit.Framework;
+    using Raven.Tests.Helpers;
+
+    public class RavenSessionExtensionTests : RavenTestBase
+    {
+        [Test]
+        public void CanGetNormalSession()
+        {
+            var store = NewDocumentStore().Initialize();
+            var session = store.OpenAsyncSession();
+
+            var storageSession = new RavenDBSynchronizedStorageSession(session, true);
+
+            var session2 = storageSession.RavenSession();
+
+            Assert.AreEqual(session, session2);
+        }
+
+        [Test]
+        public void CanGetTestableSession()
+        {
+            var store = NewDocumentStore().Initialize();
+            var session = store.OpenAsyncSession();
+
+            var storageSession = new TestableRavenStorageSession(session);
+
+            var session2 = storageSession.RavenSession();
+
+            Assert.AreEqual(session, session2);
+        }
+    }
+}
