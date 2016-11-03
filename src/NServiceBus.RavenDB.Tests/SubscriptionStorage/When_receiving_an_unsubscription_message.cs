3f79e8465d9a2b078757bf53f01894eafb5cd53e
mauroservienti <mauro@topics.it>
2016-11-03 16:34:51 +0100

    adds tests to verify the both subscribe and unsubscribe behaviors are consistent in ingoring the version

--- src/NServiceBus.RavenDB.Tests/SubscriptionStorage/When_receiving_an_unsubscription_message.cs
+++ src/NServiceBus.RavenDB.Tests/SubscriptionStorage/When_receiving_an_unsubscription_message.cs
@@ -25,4 +25,23 @@ public class When_receiving_an_unsubscribe_message : RavenDBPersistenceTestBase
 
         Assert.IsEmpty(clients);
     }
+
+    [Test]
+    public async Task All_subscription_entries_for_specified_message_types_should_be_removed_even_if_version_changes()
+    {
+        await SubscriptionIndex.CreateAsync(store);
+
+        var storage = new SubscriptionPersister(store);
+        var context = new ContextBag();
+
+        await storage.Subscribe(TestClients.ClientA, MessageTypes.MessageA, context);
+        await storage.Subscribe(TestClients.ClientA, MessageTypes.MessageB, context);
+
+        await storage.Unsubscribe(TestClients.ClientA, MessageTypes.MessageAv2, context);
+        await storage.Unsubscribe(TestClients.ClientA, MessageTypes.MessageB, context);
+
+        var clients = await storage.GetSubscriberAddressesForMessage(new[] { MessageTypes.MessageA, MessageTypes.MessageB }, context);
+
+        Assert.IsEmpty(clients);
+    }
 }
