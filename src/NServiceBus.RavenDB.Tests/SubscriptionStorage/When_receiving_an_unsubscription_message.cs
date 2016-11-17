8cb1f4cc8a57c85a3f756549f1400212faccfd02
David Boike <david.boike@gmail.com>
2016-11-17 13:13:07 -0600

    Tests to verify subscribe/unsubscribe

--- src/NServiceBus.RavenDB.Tests/SubscriptionStorage/When_receiving_an_unsubscription_message.cs
+++ src/NServiceBus.RavenDB.Tests/SubscriptionStorage/When_receiving_an_unsubscription_message.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using NServiceBus.Extensibility;
 using NServiceBus.Persistence.RavenDB;
 using NServiceBus.RavenDB.Persistence.SubscriptionStorage;
 using NServiceBus.RavenDB.Tests;
 using NServiceBus.Unicast.Subscriptions;
+using NServiceBus.Unicast.Subscriptions.MessageDrivenSubscriptions;
 using NUnit.Framework;
 
 [TestFixture]
@@ -21,38 +23,17 @@ public class When_receiving_an_unsubscribe_message : RavenDBPersistenceTestBase
         await storage.Subscribe(TestClients.ClientA, MessageTypes.MessageA, context);
         await storage.Subscribe(TestClients.ClientA, MessageTypes.MessageB, context);
 
+        // When_receiving_a_subscription_message.A_subscription_entry_should_be_added_to_the_database() ensures the Arrange above is valid
+
         await storage.Unsubscribe(TestClients.ClientA, MessageTypes.MessageA, context);
         await storage.Unsubscribe(TestClients.ClientA, MessageTypes.MessageB, context);
 
         WaitForIndexing(store);
 
-        var clients = await storage.GetSubscriberAddressesForMessage(new []{ MessageTypes.MessageA, MessageTypes.MessageB }, context);
+        var clients = await storage.GetSubscriberAddressesForMessage(new[] { MessageTypes.MessageA, MessageTypes.MessageB }, context);
 
         Assert.IsEmpty(clients);
     }
-
-    [Test]
-    public async Task All_subscription_entries_for_specified_message_types_should_be_removed_even_if_version_changes()
-    {
-        await SubscriptionIndex.CreateAsync(store);
-
-        var storage = new SubscriptionPersister(store);
-        var context = new ContextBag();
-
-        await storage.Subscribe(TestClients.ClientA, MessageTypes.MessageA, context);
-        await storage.Subscribe(TestClients.ClientA, MessageTypes.MessageB, context);
-
-        await storage.Unsubscribe(TestClients.ClientA, MessageTypes.MessageAv2, context);
-        await storage.Unsubscribe(TestClients.ClientA, MessageTypes.MessageB, context);
-
-        WaitForIndexing(store);
-
-        var clients = await storage.GetSubscriberAddressesForMessage(new[] { MessageTypes.MessageAv2, MessageTypes.MessageB }, context);
-
-        Assert.IsEmpty(clients);
-    }
-
-    // I don't trust the mirrored Subscribe/Unsubscribe test above. I think we should replace that with what I have below.
 
     [Test]
     public async Task Should_remove_matching_documents_for_any_version()
@@ -62,10 +43,18 @@ public class When_receiving_an_unsubscribe_message : RavenDBPersistenceTestBase
         var storage = new SubscriptionPersister(store);
         var context = new ContextBag();
 
-        await CreateSeedSubscription(MessageTypes.MessageA);
-        await CreateSeedSubscription(MessageTypes.MessageAv11);
-        await CreateSeedSubscription(MessageTypes.MessageAv2);
-        await CreateSeedSubscription(MessageTypes.MessageB);
+        var idA = Guid.NewGuid();
+        var idAv11 = Guid.NewGuid();
+        var idAv2 = Guid.NewGuid();
+        var idB = Guid.NewGuid();
+
+        var ids = new[] { idA, idAv11, idAv2, idB };
+        var docIds = ids.Select(id => $"Subscriptions/{id}").ToArray();
+
+        await CreateSeedSubscription(MessageTypes.MessageA, idA, TestClients.ClientA);
+        await CreateSeedSubscription(MessageTypes.MessageAv11, idAv11, TestClients.ClientA);
+        await CreateSeedSubscription(MessageTypes.MessageAv2, idAv2, TestClients.ClientA);
+        await CreateSeedSubscription(MessageTypes.MessageB, idB, TestClients.ClientA);
 
         await storage.Unsubscribe(TestClients.ClientA, MessageTypes.MessageA, context);
 
@@ -74,22 +63,32 @@ public class When_receiving_an_unsubscribe_message : RavenDBPersistenceTestBase
         var messageAClients = await storage.GetSubscriberAddressesForMessage(new[] { MessageTypes.MessageAv2, MessageTypes.MessageAv11, MessageTypes.MessageA }, context);
         var messageBClients = await storage.GetSubscriberAddressesForMessage(new[] { MessageTypes.MessageB }, context);
 
+        Subscription[] subs;
+        using (var session = store.OpenAsyncSession())
+        {
+            subs = await session.LoadAsync<Subscription>(docIds);
+        }
+
         Assert.IsEmpty(messageAClients);
-        Assert.AreEqual(1, messageBClients);
+        Assert.AreEqual(1, messageBClients.Count());
+        Assert.AreEqual(0, subs[0].Subscribers.Count); // A
+        Assert.AreEqual(0, subs[0].Subscribers.Count); // Av11
+        Assert.AreEqual(0, subs[0].Subscribers.Count); // Av2
+        Assert.AreEqual(1, subs[0].Subscribers.Count); // B
     }
 
-    private Task CreateSeedSubscription(MessageType msgType, Guid? id = null)
+    Task CreateSeedSubscription(MessageType msgType, Guid id, params Subscriber[] subscribers)
     {
         var sub = new Subscription();
         sub.MessageType = msgType;
-        sub.Subscribers.Add(new SubscriptionClient
+        sub.Subscribers.AddRange(subscribers.Select(s => new SubscriptionClient
         {
-            Endpoint = "TestEndpoint",
-            TransportAddress = "TestEndpoint@Machine"
-        });
+            Endpoint = s.Endpoint,
+            TransportAddress = s.TransportAddress
+        }));
 
-        var docId = $"Subscriptions/{id ?? Guid.NewGuid()}";
+        var docId = $"Subscriptions/{id}";
 
-        return RavenUtils.StoreAsType(store, docId, typeof(Subscription), msgType);
+        return RavenUtils.StoreAsType(store, docId, typeof(Subscription), sub);
     }
 }
