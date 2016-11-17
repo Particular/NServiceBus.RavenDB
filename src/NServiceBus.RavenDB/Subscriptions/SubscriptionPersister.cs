b81884af19c389e68d8be6c23ae1997bde7b3e7d
David Boike <david.boike@gmail.com>
2016-11-17 14:51:00 -0600

    potentially dangerous hack

--- src/NServiceBus.RavenDB/Subscriptions/SubscriptionPersister.cs
+++ src/NServiceBus.RavenDB/Subscriptions/SubscriptionPersister.cs
@@ -141,6 +141,7 @@ namespace NServiceBus.Persistence.RavenDB
                         {
                             var ret = session.Advanced
                                 .AsyncDocumentQuery<Subscription>($"{subscriptionCollectionName}Index")
+                                .WaitForNonStaleResultsAsOfLastWrite()
                                 .Where($"MessageType: \"{messageType.TypeName}, Version=*\"")
                                 .LazilyAsync(null);
                             lazyDocuments.Add(ret);
