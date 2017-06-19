2f3af8344316a14dd8fdae5d23fc74e4fae0b57b
Mauro Servienti <mauro@topics.it>
2017-06-19 18:27:54 +0200

    Test that subscriptions store can ignore message versions

---
+++ src/NServiceBus.RavenDB.Tests/SubscriptionStorage/When_receiving_multiple_subscription_messages_with_defferent_versions.cs
@@ -0,0 +1,48 @@
+using System.Linq;
+using System.Threading.Tasks;
+using NServiceBus.Extensibility;
+using NServiceBus.Persistence.RavenDB;
+using NServiceBus.RavenDB.Persistence.SubscriptionStorage;
+using NServiceBus.RavenDB.Tests;
+using NServiceBus.Unicast.Subscriptions;
+using NServiceBus.Unicast.Subscriptions.MessageDrivenSubscriptions;
+using NUnit.Framework;
+using Raven.Client;
+
+[TestFixture]
+public class When_subscriptions_versioning_is_disabled : RavenDBPersistenceTestBase
+{
+    [Test]
+    public async Task should_ignore_message_version()
+    {
+        var subscriberAddress_v1 = "v1@localhost";
+        var subscriberAddress_v2 = "v2@localhost";
+        var messageType_v1 = new MessageType("SomeMessageType", "1.0.0.0");
+        var messageType_v2 = new MessageType("SomeMessageType", "2.0.0.0");
+        var subscriber_v1 = new Subscriber(subscriberAddress_v1, "some_endpoint_name");
+        var subscriber_v2 = new Subscriber(subscriberAddress_v2, "another_endpoint_name");
+
+        var storage = new SubscriptionPersister(store)
+        {
+            DisableSubscriptionsVersioning = true
+        };
+
+        await storage.Subscribe(subscriber_v1, messageType_v1, new ContextBag());
+        await storage.Subscribe(subscriber_v2, messageType_v2, new ContextBag());
+ 
+        var subscribers_looked_up_by_v1 = (await storage.GetSubscriberAddressesForMessage(new[]
+        {
+            messageType_v1
+        }, new ContextBag())).ToArray();
+
+        var subscribers_looked_up_by_v2 = (await storage.GetSubscriberAddressesForMessage(new[]
+        {
+            messageType_v2
+        }, new ContextBag())).ToArray();
+
+        Assert.AreEqual(2, subscribers_looked_up_by_v1.Length);
+        Assert.AreEqual(2, subscribers_looked_up_by_v2.Length);
+        //Assert.AreEqual(subscriberAddress, subscribers[0].TransportAddress);
+        //Assert.AreEqual("new_endpoint_name", subscribers[0].Endpoint);
+    }
+}
