b5a7c61e74a0470f7f7481827f7a2289a59b82db
Brandon Ording <bording@gmail.com>
2019-01-11 14:57:33 -0500

    Switch to Particular.Approvals

--- src/NServiceBus.RavenDB.Tests/API/APIApprovals.cs
+++ src/NServiceBus.RavenDB.Tests/API/APIApprovals.cs
@@ -1,18 +1,17 @@
 ﻿namespace NServiceBus.RavenDB.Tests.API
 {
-    using System.Runtime.CompilerServices;
     using NUnit.Framework;
+    using Particular.Approvals;
     using PublicApiGenerator;
 
     [TestFixture]
     class APIApprovals
     {
         [Test]
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public void ApproveRavenDbPersistence()
         {
             var publicApi = ApiGenerator.GeneratePublicApi(typeof(RavenDBPersistence).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
-            TestApprover.Verify(publicApi);
+            Approver.Verify(publicApi);
         }
     }
 }
