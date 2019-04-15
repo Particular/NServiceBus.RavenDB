595a35a03d6d83ce21c9402244c0003deddd3919
David Boike <david.boike@gmail.com>
2019-04-15 13:40:58 -0500

    Fixed bug from conversion of Lucene query to expression

--- src/NServiceBus.RavenDB/Outbox/OutboxRecordsCleaner.cs
+++ src/NServiceBus.RavenDB/Outbox/OutboxRecordsCleaner.cs
@@ -18,7 +18,7 @@
         public async Task RemoveEntriesOlderThan(DateTime dateTime, CancellationToken cancellationToken = default(CancellationToken))
         {
             var options = new QueryOperationOptions { AllowStale = true };
-            var deleteOp = new DeleteByQueryOperation<OutboxRecord, OutboxRecordsIndex>(record => record.Dispatched && record.DispatchedAt >= dateTime, options);
+            var deleteOp = new DeleteByQueryOperation<OutboxRecord, OutboxRecordsIndex>(record => record.Dispatched && record.DispatchedAt <= dateTime, options);
 
             var operation = await documentStore.Operations.SendAsync(deleteOp, token: cancellationToken).ConfigureAwait(false);
 
