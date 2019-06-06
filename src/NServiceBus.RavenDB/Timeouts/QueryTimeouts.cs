5b9e076e1fc4365fa1641810687e4399bdcfe763
David Boike <david.boike@gmail.com>
2019-06-06 11:30:29 -0500

    Inspection

--- src/NServiceBus.RavenDB/Timeouts/QueryTimeouts.cs
+++ src/NServiceBus.RavenDB/Timeouts/QueryTimeouts.cs
@@ -3,7 +3,6 @@ namespace NServiceBus.Persistence.RavenDB
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Runtime.InteropServices;
     using System.Threading;
     using System.Threading.Tasks;
     using NServiceBus.Logging;
