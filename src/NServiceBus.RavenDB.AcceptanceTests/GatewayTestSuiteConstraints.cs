725163c1519fc0949462d48a8a21e21b7d2acac9
mauroservienti <mauro.servienti@gmail.com>
2020-02-04 16:10:31 +0100

    adjust gateway configuration to new test suite API

--- src/NServiceBus.RavenDB.AcceptanceTests/GatewayTestSuiteConstraints.cs
+++ src/NServiceBus.RavenDB.AcceptanceTests/GatewayTestSuiteConstraints.cs
@@ -1,12 +1,31 @@
 ﻿namespace NServiceBus.Gateway.AcceptanceTests
 {
-    using NServiceBus.RavenDB.AcceptanceTests;
+    using NServiceBus.AcceptanceTesting.Support;
+    using System.Threading.Tasks;
 
     public partial class GatewayTestSuiteConstraints
     {
-        public IConfigureGatewayPersitenceExecution CreatePersistenceConfiguration()
+        public Task ConfigureDeduplicationStorage(string endpointName, EndpointConfiguration configuration, RunSettings settings)
         {
-            return new ConfigureRavenDBGatewayPersitence();
+            var documentStore = ConfigureEndpointRavenDBPersistence.GetDocumentStore();
+
+            databaseName = documentStore.Database;
+
+
+            configuration.UsePersistence<RavenDBPersistence, StorageType.GatewayDeduplication>()
+                .DoNotSetupDatabasePermissions()
+                .SetDefaultDocumentStore(documentStore);
+
+            configuration.Gateway();
+
+            return Task.FromResult(false);
         }
+
+        public Task Cleanup()
+        {
+            return ConfigureEndpointRavenDBPersistence.DeleteDatabase(databaseName);
+        }
+
+        string databaseName;
     }
 }
