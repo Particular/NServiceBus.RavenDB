097fb00ae8441f739d7a82177c0fd7c4b69f7494
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-08-06 13:48:55 +0200

    Add persistence tests package for RavenDB (#534)
    * add Test.snk
    
    * add new test project
    
    * setup configuration
    
    * update test sources package reference
    
    * set incoming message for tenant aware session creator
    
    * update to latest version
    
    * remove unused code
    
    * enable outbox tests
    
    * disable finder tests
    
    * approve InternalsVisibleTo for test project
    
    * configure SessionAdapter
    
    * disable DTC tests as RavenDB no longer supports DTC transactions
    
    * provide new persistence configuration argument
    
    * configure both optimistic and pessimistic test variants
    
    * do not reduce the actual lock timeout
    
    * update package dependency
    
    * update test sources
    
    * Remove failing test
    
    * Tweaks
    
    * update to 7.4
    
    Co-authored-by: Brandon Ording <bording@gmail.com>

---
+++ src/NServiceBus.RavenDB.PersistenceTests/PersistenceTestsConfiguration.cs
@@ -0,0 +1,119 @@
+﻿namespace NServiceBus.PersistenceTesting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using NServiceBus.Extensibility;
+    using NServiceBus.Outbox;
+    using NServiceBus.Persistence;
+    using NServiceBus.Sagas;
+    using NServiceBus.Persistence.RavenDB;
+    using NServiceBus.Transport;
+    using Raven.Client.Documents;
+    using Raven.Client.ServerWide;
+    using Raven.Client.ServerWide.Operations;
+
+    public partial class PersistenceTestsConfiguration
+    {
+        static PersistenceTestsConfiguration()
+        {
+            var optimisticConcurrencyConfiguration = new SagaPersistenceConfiguration();
+            optimisticConcurrencyConfiguration.UsePessimisticLocking(false);
+            var pessimisticLockingConfiguration = new SagaPersistenceConfiguration();
+            pessimisticLockingConfiguration.UsePessimisticLocking(true);
+
+            SagaVariants = new[]
+            {
+                new TestVariant(optimisticConcurrencyConfiguration),
+                new TestVariant(pessimisticLockingConfiguration)
+            };
+            OutboxVariants = new[]
+            {
+                new TestVariant(optimisticConcurrencyConfiguration)
+            };
+        }
+
+        DocumentStore documentStore;
+
+        public bool SupportsDtc => false;
+
+        public bool SupportsOutbox => true;
+
+        public bool SupportsFinders => false;
+
+        public bool SupportsPessimisticConcurrency { get; private set; }
+
+        public ISagaIdGenerator SagaIdGenerator { get; private set; }
+
+        public ISagaPersister SagaStorage { get; private set; }
+
+        public ISynchronizedStorage SynchronizedStorage { get; private set; }
+
+        public ISynchronizedStorageAdapter SynchronizedStorageAdapter { get; private set; }
+
+        public IOutboxStorage OutboxStorage { get; private set; }
+
+        public Task Configure()
+        {
+            GetContextBagForOutbox = GetContextBagForSagaStorage = () =>
+            {
+                var context = new ContextBag();
+                context.Set(new IncomingMessage("native id", new Dictionary<string, string>(), new byte[0]));
+                return context;
+            };
+
+            SagaIdGenerator = new DefaultSagaIdGenerator();
+            var sagaPersistenceConfiguration = Variant.Values[0] as SagaPersistenceConfiguration;
+            SupportsPessimisticConcurrency = sagaPersistenceConfiguration.EnablePessimisticLocking;
+            if (SessionTimeout.HasValue)
+            {
+                sagaPersistenceConfiguration.SetPessimisticLeaseLockAcquisitionTimeout(SessionTimeout.Value);
+            }
+            SagaStorage = new SagaPersister(sagaPersistenceConfiguration);
+
+            var dbName = Guid.NewGuid().ToString();
+            var urls = Environment.GetEnvironmentVariable("CommaSeparatedRavenClusterUrls") ?? "http://localhost:8080";
+            documentStore = new DocumentStore
+            {
+                Urls = urls.Split(','),
+                Database = dbName
+            };
+            documentStore.Initialize();
+            var dbRecord = new DatabaseRecord(dbName);
+            documentStore.Maintenance.Server.Send(new CreateDatabaseOperation(dbRecord));
+
+            IOpenTenantAwareRavenSessions sessionCreator = new OpenRavenSessionByDatabaseName(new DocumentStoreWrapper(documentStore));
+            SynchronizedStorage = new RavenDBSynchronizedStorage(sessionCreator);
+            SynchronizedStorageAdapter = new RavenDBSynchronizedStorageAdapter();
+
+            OutboxStorage = new OutboxPersister(documentStore.Database, sessionCreator, RavenDbOutboxStorage.DeduplicationDataTTLDefault);
+            return Task.CompletedTask;
+        }
+
+        public async Task Cleanup()
+        {
+            // Periodically the delete will throw an exception because Raven has the database locked
+            // To solve this we have a retry loop with a delay
+            var triesLeft = 3;
+
+            while (triesLeft-- > 0)
+            {
+                try
+                {
+                    documentStore.Maintenance.Server.Send(new DeleteDatabasesOperation(documentStore.Database, hardDelete: true));
+                    documentStore.Dispose();
+                    break;
+                }
+                catch
+                {
+                    if (triesLeft == 0)
+                    {
+                        throw;
+                    }
+
+                    await Task.Delay(250);
+                }
+            }
+        }
+    }
+}
