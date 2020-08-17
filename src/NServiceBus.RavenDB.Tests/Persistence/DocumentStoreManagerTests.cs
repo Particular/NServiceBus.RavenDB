335e23b0ffef16d20afbb9483fa166bbda47fbff
mauroservienti <mauro.servienti@gmail.com>
2020-08-17 14:33:02 +0200

    Remove gateway tests

--- src/NServiceBus.RavenDB.Tests/Persistence/DocumentStoreManagerTests.cs
+++ src/NServiceBus.RavenDB.Tests/Persistence/DocumentStoreManagerTests.cs
@@ -20,9 +20,6 @@
                 settings.Set("NServiceBus.Transport.TransportInfrastructure", new FakeRavenDBTransportInfrastructure(TransportTransactionMode.None));
                 settings.Set("Endpoint.SendOnly", true);
 
-#pragma warning disable 618
-                DocumentStoreManager.SetDocumentStore<StorageType.GatewayDeduplication>(settings, db.NewStore("GatewayDeduplication"));
-#pragma warning restore 618
                 DocumentStoreManager.SetDocumentStore<StorageType.Outbox>(settings, db.NewStore("Outbox"));
                 DocumentStoreManager.SetDocumentStore<StorageType.Sagas>(settings, db.NewStore("Sagas"));
                 DocumentStoreManager.SetDocumentStore<StorageType.Subscriptions>(settings, db.NewStore("Subscriptions"));
@@ -31,9 +28,6 @@
 
                 var readOnly = settings as ReadOnlySettings;
 
-#pragma warning disable 618
-                Assert.AreEqual("GatewayDeduplication", DocumentStoreManager.GetDocumentStore<StorageType.GatewayDeduplication>(readOnly, null).Identifier);
-#pragma warning restore 618
                 Assert.AreEqual("Outbox", DocumentStoreManager.GetDocumentStore<StorageType.Outbox>(readOnly, null).Identifier);
                 Assert.AreEqual("Sagas", DocumentStoreManager.GetDocumentStore<StorageType.Sagas>(readOnly, null).Identifier);
                 Assert.AreEqual("Subscriptions", DocumentStoreManager.GetDocumentStore<StorageType.Subscriptions>(readOnly, null).Identifier);
