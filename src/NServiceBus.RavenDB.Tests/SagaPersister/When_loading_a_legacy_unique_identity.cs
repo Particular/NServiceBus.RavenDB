b48ab5295241c863a7e8ba57cad97683e5485004
Ramon Smits <ramon.smits@gmail.com>
2020-09-17 17:20:00 +0200

    Fix for incomplete v5 to v6 saga data migration and fix v6 saga data.

--- src/NServiceBus.RavenDB.Tests/SagaPersister/When_loading_a_legacy_unique_identity.cs
+++ src/NServiceBus.RavenDB.Tests/SagaPersister/When_loading_a_legacy_unique_identity.cs
@@ -36,6 +36,7 @@ class When_loading_a_saga_with_legacy_unique_identity : RavenDBPersistenceTestBa
             var saga = await persister.Get<SagaWithUniqueProperty>("UniqueString", unique, synchronizedSession, options);
 
             Assert.IsNotNull(saga, "Saga is null");
+            Assert.AreNotEqual(Guid.Empty, saga.Id, "Id is Guid.Empty");
 
             await persister.Complete(saga, synchronizedSession, options);
             await session.SaveChangesAsync().ConfigureAwait(false);
