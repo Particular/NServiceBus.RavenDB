f48cfe2f39dbebaca69d79367bf3cba44e6ce18e
Ramon Smits <ramon.smits@gmail.com>
2020-10-26 17:30:20 +0100

    Removing existing timeout code.

---
+++ src/NServiceBus.RavenDB/obsoletes-v7.cs
@@ -0,0 +1,29 @@
+﻿#pragma warning disable 1591
+
+namespace NServiceBus
+{
+    using System;
+    using Settings;
+    using Raven.Client.Documents;
+
+    [ObsoleteEx(
+        Message = "Timeout manager removed. Timeout storage configuration can be removed.",
+        TreatAsErrorFromVersion = "7",
+        RemoveInVersion = "8"
+    )]
+    public static class RavenDbTimeoutSettingsExtensions
+    {
+        public static PersistenceExtensions<RavenDBPersistence> UseDocumentStoreForTimeouts(this PersistenceExtensions<RavenDBPersistence> cfg, IDocumentStore documentStore)
+        {
+            throw new NotImplementedException();
+        }
+        public static PersistenceExtensions<RavenDBPersistence> UseDocumentStoreForTimeouts(this PersistenceExtensions<RavenDBPersistence> cfg, Func<ReadOnlySettings, IDocumentStore> storeCreator)
+        {
+            throw new NotImplementedException();
+        }
+        public static PersistenceExtensions<RavenDBPersistence> UseDocumentStoreForTimeouts(this PersistenceExtensions<RavenDBPersistence> cfg, Func<ReadOnlySettings, IServiceProvider, IDocumentStore> storeCreator)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
