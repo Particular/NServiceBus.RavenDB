1ebb6543bbdf71052222251f4fe7150be4503e36
danielmarbach <daniel.marbach@openplace.net>
2021-11-30 13:05:48 +0100

    Make sure the tests exclude their own assembly

--- src/NServiceBus.RavenDB.Tests/When_providing_a_custom_document_store.cs
+++ src/NServiceBus.RavenDB.Tests/When_providing_a_custom_document_store.cs
@@ -13,7 +13,7 @@
         {
             var endpointConfiguration = new EndpointConfiguration("custom-docstore-endpoint");
 
-            endpointConfiguration.AssemblyScanner().ExcludeAssemblies("NServiceBus.RavenDB.Tests", "NServiceBus.RavenDB.ClusterWide.Tests");
+            endpointConfiguration.AssemblyScanner().ExcludeAssemblies($"{GetType().Assembly.GetName().Name}");
             endpointConfiguration.UseTransport(new LearningTransport());
             endpointConfiguration.EnableOutbox();
 
