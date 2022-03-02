4a76bd77a53b5fd6b742a4d6a995daf1c86a916e
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-03-02 15:30:06 +0100

    ensure assembly is loaded

---
+++ src/NServiceBus.RavenDB.AcceptanceTests/TestSetup.cs
@@ -0,0 +1,13 @@
+﻿using NServiceBus;
+using NUnit.Framework;
+
+[SetUpFixture]
+public class TestSetup
+{
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        // ensure the RavenDB assembly is loaded into the AppDomain because it needs its features to be scanned to work properly.
+        typeof(RavenDBPersistence).ToString();
+    }
+}
