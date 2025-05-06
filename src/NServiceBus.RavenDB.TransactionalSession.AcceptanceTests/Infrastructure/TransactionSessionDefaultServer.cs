2329e4017ae795f38fec09a550f99845d328c8a5
Andreas Öhlund <andreas.ohlund@particular.net>
2025-05-06 09:23:18 +0200

    Fix tx server

--- src/NServiceBus.RavenDB.TransactionalSession.AcceptanceTests/Infrastructure/TransactionSessionDefaultServer.cs
+++ src/NServiceBus.RavenDB.TransactionalSession.AcceptanceTests/Infrastructure/TransactionSessionDefaultServer.cs
@@ -1,6 +1,20 @@
 namespace NServiceBus.TransactionalSession.AcceptanceTests
 {
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting.Support;
+    using Configuration.AdvancedExtensibility;
+
     public class TransactionSessionDefaultServer : DefaultServer
     {
+        public override async Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointCustomization, Func<EndpointConfiguration, Task> configurationBuilderCustomization)
+        {
+            var endpointConfiguration = await base.GetConfiguration(runDescriptor, endpointCustomization, configurationBuilderCustomization);
+
+            endpointConfiguration.GetSettings().Get<PersistenceExtensions<RavenDBPersistence>>()
+                .EnableTransactionalSession();
+
+            return endpointConfiguration;
+        }
     }
 }
