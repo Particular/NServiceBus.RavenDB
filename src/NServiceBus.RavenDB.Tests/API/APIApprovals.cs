48a757e34d6696da9f322f0626677ca789267643
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-10-20 11:36:40 +0200

    Adjust to seam

--- src/NServiceBus.RavenDB.Tests/API/APIApprovals.cs
+++ src/NServiceBus.RavenDB.Tests/API/APIApprovals.cs
@@ -12,7 +12,7 @@
         {
             var publicApi = typeof(RavenDBPersistence).Assembly.GeneratePublicApi(new ApiGeneratorOptions
             {
-                ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" }
+                ExcludeAttributes = ["System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute"]
             });
             Approver.Verify(publicApi);
         }
